dd94bd332bc022432d4db93d46d144abc0fa5d04
risefoundation <resilientinitiativesfoundation@gmail.com>
2021-05-14 04:40:03 +0530

    navbar changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 //import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import './Navbar.css';
-import Logo from './pollinate-logo.png';
+import Logo from './header.png';
 import Dropdown from 'react-bootstrap/Dropdown'
 import main from './main.png';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -11,10 +11,10 @@ import {Link} from 'react-router-dom';
 function Navbar() {
     return (
         <div>
-   <nav class="navbar fixed-top navbar-expand-lg navbar-dark navbar-header ">
+   <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-info navbar-header ">
   <div class="container-fluid">
   
-    <Link class="navbar-brand" to="/"><img src={Logo} width="200px"/></Link>
+    <Link class="navbar-brand" to="/"><img src={Logo} width="50px"/></Link>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon text-end"></span>
